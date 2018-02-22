a950dc200847ab917bdf9cfe86393d609bc5d144
Joe Grice <joe.grice@hotmail.co.uk>
2018-02-22 19:27:28 +0000

    Added firebase registering and loggin in

---
+++ app/src/main/java/travelmate/com/travelmateapp/RegisterActivity.java
@@ -0,0 +1,100 @@
+package travelmate.com.travelmateapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView progressBarText;
+    private CardView progressCardView;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+        TextView textViewSignin = findViewById(R.id.textViewSignIn);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        progressCardView = findViewById(R.id.regProgressCardView);
+        progressBarText = findViewById(R.id.regProgressBarText);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+    }
+
+    public void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressCardView.setVisibility(View.VISIBLE);
+        progressBarText.setText("Registering User...");
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressCardView.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            Log.d(TAG, "createUserWithEmail:success");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonRegister:
+                registerUser();
+                break;
+            case R.id.textViewSignIn:
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+    }
+}
