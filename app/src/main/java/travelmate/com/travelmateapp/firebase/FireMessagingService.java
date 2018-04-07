6b42705e217aa3fb704d27f3d15f26c6adf00b89
Joe Grice <joe.grice@hotmail.co.uk>
2018-04-07 15:10:47 +0100

    Added basic handling notifications

--- app/src/main/java/travelmate/com/travelmateapp/firebase/FireMessagingService.java
+++ app/src/main/java/travelmate/com/travelmateapp/firebase/FireMessagingService.java
@@ -1,6 +1,6 @@
 package travelmate.com.travelmateapp.firebase;
 
-import android.util.Log;
+import android.content.Intent;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -10,36 +10,16 @@ import com.google.firebase.messaging.RemoteMessage;
  */
 
 public class FireMessagingService extends FirebaseMessagingService {
+
     private static final String TAG = FireMessagingService.class.getSimpleName();
 
     @Override
+    public void handleIntent(Intent intent) {
+        super.handleIntent(intent);
+    }
+
+    @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                Log.d(TAG, remoteMessage.getData().toString());
-            } else {
-                // Handle message within 10 seconds
-                Log.d(TAG, remoteMessage.getData().toString());
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        super.onMessageReceived(remoteMessage);
     }
 }
